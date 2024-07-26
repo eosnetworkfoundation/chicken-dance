3b7b8d2f6bd036775cee17a78e909c982a5efac1
Zach Butler <zach.butler@eosnetwork.com>
2024-07-26 19:35:24 -0400

    ShellCheck SC2086: Double quote to prevent globbing and word splitting.

--- replay-client/install-nodeos.sh
+++ replay-client/install-nodeos.sh
@@ -6,7 +6,7 @@ LEAP_VERSION="${1}"
 OS="ubuntu22.04"
 v4_DEB_FILE="leap_""${LEAP_VERSION}"-"${OS}""_amd64.deb"
 v5_DEB_FILE="leap_""${LEAP_VERSION}""_amd64.deb"
-if [ ${LEAP_VERSION:0:1} == "4" ]; then
+if [ "${LEAP_VERSION:0:1}" == "4" ]; then
     DEB_URL="https://github.com/AntelopeIO/leap/releases/download/v""${LEAP_VERSION}"/"${v4_DEB_FILE}"
     DEB_FILE=${v4_DEB_FILE}
 else
@@ -19,13 +19,13 @@ fi
 for not_needed_deb_file in "${HOME:?}"/leap_*.deb; do
     if [ "${not_needed_deb_file}" != "${HOME:?}"/"${DEB_FILE}" ]; then
         echo "Removing not needed deb ${not_needed_deb_file}"
-        rm -rf ${not_needed_deb_file}
+        rm -rf "${not_needed_deb_file}"
     fi
 done
 
 # download file if needed
 if [ ! -f "${HOME:?}"/"${DEB_FILE}" ]; then
-    wget --directory-prefix=${HOME} "${DEB_URL}" 2> /dev/null
+    wget --directory-prefix="${HOME}" "${DEB_URL}" 2> /dev/null
 fi
 
 # install nodeos locally
